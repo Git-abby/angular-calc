3031c138745702bb2ad43ed9ad027f29aad0a183
Git-abby <malekabrar56@gmail.com>
2024-10-27 12:12:15 -0400

    chore: functions/methods added

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrl: './calculator.component.css',
 })
 export class CalculatorComponent {
+  stringToEval: string = '';
 
+  //methods to take input from the keys
+  to_get_keys(num: string) {
+  
+    this.stringToEval = this.stringToEval + num;
+    console.log(this.stringToEval);
+  }
+  get_answer() {
+    //condition for catching undefined stringToEval
+    if (this.stringToEval != '') {
+      this.stringToEval = eval(this.stringToEval);
+      console.log(this.stringToEval);
+    }
+  }
+  clear_output() {
+    this.stringToEval = '';
+  }
 }
